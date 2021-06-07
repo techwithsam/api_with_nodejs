b7c2826f4048f45694b6a43ceb17938d03835f8c
Samuel <samuelbeebest@gmail.com>
2021-06-07 23:58:23 +0100

    new update

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,12 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-   res.send('Hello World Samuel!!!');
+const courses = [
+  { id: 1, name: 'course1' },
+  { id: 2, name: 'course2' },
+  { id: 3, name: 'course3' },
+];
+
+app.get("/", (req, res) => {
+  res.send("Hello World Samuel!!!");
 });
 
-app.get('/api/courses', (req, res) => {
-   res.send([1,2,3]);
+app.get("/api/courses", (req, res) => {
+  res.send(courses);
 });
 
-app.listen(8000, () => console.log('Listening on port 8000...'))
+app.get('/api/courses/:id', (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send('The course with the given ID was not found!');
+  res.send(course);
+});
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
